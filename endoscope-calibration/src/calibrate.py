661f9904e70e222cd768d32313ba24c517462fd2
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2019-03-06 06:10:54 +0000

    Add 3D render generation

--- endoscope-calibration/src/calibrate.py
+++ endoscope-calibration/src/calibrate.py
@@ -69,75 +69,6 @@ def calculate_camera_intrinsics(points_3d, points_2d, sample_frame):
     return cam_matrix, dist_coeffs, rvecs, tvecs
 
 
-def calc_best_homography(points_1_cart, points_2_cart):
-    """
-    Uses Direct Linear Transform (DLT) algorithm to calculate the best homography mapping
-    `cart_points_1` to corresponding `cart_points_2`
-
-    d - number of dimensions
-    n - number of points
-
-    :param points_1_cart: `d x n` matrix
-    :param points_2_cart: `d x n` matrix
-    :return:
-    """
-    n = points_1_cart.shape[1]
-    points_1_hom = util.to_homog(points_1_cart)
-    points_2_hom = util.to_homog(points_2_cart)
-
-    A = np.zeros((2 * n, 9))
-    for i in range(n):
-        pt1 = points_1_hom[:, i]
-        pt2 = points_2_hom[:, i]
-        A[2 * i, 3:6] = -pt1
-        A[2 * i, 6:9] = pt1 * pt2[1]
-        A[2 * i + 1, 0:3] = pt1
-        A[2 * i + 1, 6:9] = -pt1 * pt2[0]
-
-    U, s, VT = np.linalg.svd(A)
-    h = VT.T[:, -1]
-    H = h.reshape(3, 3)
-    return H
-
-
-# Goal of function is to estimate pose of plane relative to camera (extrinsic matrix)
-# given points in image xImCart, points in world XCart and intrinsic matrix K
-def estimate_plane_pos(points_cart_3d, points_cart_2d, cam_matrix):
-    img_points_hom = util.to_homog(points_cart_2d)
-    cam_points_hom = np.linalg.inv(cam_matrix) @ img_points_hom
-    H = calc_best_homography(points_cart_3d[0:2, :], util.to_cart(cam_points_hom))
-
-    # Estimate first two columns of rotation matrix R from the first two
-    # columns of H using the SVD
-    R = np.zeros((3, 3))
-    U, L, VT = np.linalg.svd(H[:, 0:2])
-    R[:, 0:2] = U @ np.eye(3)[:, 0:2] @ VT
-
-    # Estimate the third column of the rotation matrix by taking the cross
-    # product of the first two columns
-    R[:, 2] = np.cross(R[:, 0], R[:, 1])
-    det = np.linalg.det(R)
-    if det < 0:
-        R[:, 2] *= -1
-
-    # Estimate the translation t by finding the appropriate scaling factor k
-    # and applying it to the third column of H
-    sum_ = 0
-    for i in range(3):
-        for j in range(2):
-            sum_ += H[i, j] / R[i, j]
-    t = H[:, 2] / (sum_ / 6)
-
-    # TO DO: Check whether t_z is negative - if it is then multiply t by -1 and
-    # the first two columns of R by -1.
-    if t[2] < 0:
-        t *= -1
-        R[:, 0] *= -1
-        R[:, 1] *= -1
-
-    return R, t
-
-
 def calc_rigid_body_transform(from_pts, to_pts):
     """
     Calculates the transform between two point clouds - note that no
@@ -155,13 +86,15 @@ def calc_rigid_body_transform(from_pts, to_pts):
 
     R, _, _ = calc_rot_without_correspondence(from_pts, to_pts)
     t = to_cent - from_cent
-    T2 = util.to_transform(R, t)
 
     # Recall that our R only works for zero-centred point
     # clouds, so need to include that into transform
     T1 = util.to_transform(trans_vec=-from_cent)
+    T2 = util.to_transform(rot_matrix=R)
+    T3 = util.to_transform(trans_vec=from_cent)
+    T4 = util.to_transform(trans_vec=t)
 
-    return T2 @ T1
+    return T4 @ T3 @ T2 @ T1
 
 
 def calc_rot_without_correspondence(from_pts, to_pts):
--- endoscope-calibration/src/render.py
+++ endoscope-calibration/src/render.py
@@ -1,32 +1,59 @@
 from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
 from panda3d.core import *
+from time import sleep
+from os import path
 import numpy as np
+import cv2 as cv
+import tempfile
+import sys
+import os
+
+# Necessary to make sure this code works when imported into a Jupyter notebook
+script_dir = path.dirname(path.realpath(__file__))
+sys.path.append(script_dir)
+
+# Our local modules
+import util
+import transform as tf
 
 
 class RenderApp(ShowBase):
 
-    def __init__(self):
-        ShowBase.__init__(self, )  # windowType='offscreen')
+    def __init__(self, width, height, headless=False):
+        # if headless:
+        #     ShowBase.__init__(self, windowType='offscreen')
+        # else:
+        #     ShowBase.__init__(self)
+        ShowBase.__init__(self)
 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
+        self.width = width
+        self.height = height
+
+        # if headless:
+        #     self.win.setSize(self.width, self.height)
+        # else:
+        #     props = WindowProperties()
+        #     props.setTitle("Endoscope Position Preview")
+        #     props.setSize(self.width, self.height)
+        #     props.setFixedSize(True)
+        #     self.win.requestProperties(props)
+        # TODO: In headless mode, the window size is incorrect - try to fix it to have the correct size.
+        props = WindowProperties()
+        props.setTitle("3D render")
+        props.setSize(self.width, self.height)
+        props.setFixedSize(True)
+        self.win.requestProperties(props)
 
         self.temp = 0
+        self.realCamera = None
 
-        # Apply scale and position transforms on the model.
-        # self.scene.setScale(0.25, 0.25, 0.25)
-        # self.scene.setPos(-8, 42, 0)
-
-    def init_scene(self):
+    def init_scene(self, R, t, cam_matrix, phantom_model_path, endoscope_markers_path=None):
         render = self.render
 
         dlight = DirectionalLight('dlight')
-        dlight.setColor(VBase4(0.8, 0.8, 0.5, 1))
+        dlight.setColor(VBase4(0.8, 0.8, 0.5, 1) * 0.5)
         dlnp = render.attachNewNode(dlight)
-        dlnp.setHpr(0, -60, 0)
+        dlnp.setHpr(0, 60, 0)
         render.setLight(dlnp)
 
         alight = AmbientLight('alight')
@@ -34,33 +61,96 @@ class RenderApp(ShowBase):
         alnp = render.attachNewNode(alight)
         render.setLight(alnp)
 
-        phantom_model = self.loader.loadModel('../data/endo_phantom/mesh2_phantom.stl')
+        # Load phantom model and set material
+        phantom_model = self.loader.loadModel(phantom_model_path)
         phantom_model.reparentTo(render)
 
-        # print(phantom_model.getPos())
+        myMaterial = Material()
+        myMaterial.setShininess(10.0)  # Make this material shiny
+        myMaterial.setAmbient((0, 0, 0, 1))  # Make this material blue
+        phantom_model.setMaterial(myMaterial)
 
-        markers_model = self.loader.loadModel('../data/endo_phantom/mesh1_endoscope_markers.stl')
-        markers_model.reparentTo(render)
+        # Load endoscope markers - these are optional because they might not even be visible in the shot, and they don't
+        # give us any useful information about the scene.
+        if endoscope_markers_path:
+            markers_model = self.loader.loadModel(endoscope_markers_path)
+            markers_model.reparentTo(render)
 
-        self.taskMgr.add(self.spin_camera_task, "SpinCameraTask")
+        self.disableMouse()
 
-    def spin_camera_task(self, task):
-        angleDegrees = task.time * 6.0
-        angleRadians = angleDegrees * (np.pi / 180.0)
-        # self.camera.setPos(0, 0, task.time / 10)
-        # self.camera.setPos(20 * np.sin(angleRadians), -20.0 * np.cos(angleRadians), 3)
-        # self.camera.setHpr(angleDegrees, 0, 0)
-        return Task.cont
+        # Set camera pose - not that we need to add 90 to pitch for the camera to be aligned correctly.
+        eulerDegrees = np.rad2deg(tf.rot_matrix_to_euler(R))
+        adjustEulerDegrees = np.array([0., 90., 0.]) + eulerDegrees
+        self.camera.setHpr(adjustEulerDegrees[0], adjustEulerDegrees[1], adjustEulerDegrees[2])
+        self.camera.setPos(t[0], t[1], t[2])
+
+        # Find camera object and set aspect ratio and focal length
+        for obj in self.camera.getChildren():
+            self.realCamera = obj.node()
+        lens = self.realCamera.getLens()
+        aspect_ratio = self.width / self.height
+        f_x, f_y = cam_matrix[0, 0], cam_matrix[1, 1]
+        focal_length = (f_x / self.width + f_y / self.height) / 2
+        # focal_length = f_x / self.width
+        lens.setAspectRatio(aspect_ratio)
+        lens.setFocalLength(focal_length)
+
+
+def generate_render(width, height, T_cam_to_world, cam_matrix, phantom_model_path, endoscope_markers_path):
+    # Initialise the render
+    R, t = tf.from_transform(T_cam_to_world)
+    app = RenderApp(width=width, height=height, headless=True)
+    app.init_scene(R=R, t=t, cam_matrix=cam_matrix, phantom_model_path=phantom_model_path,
+                   endoscope_markers_path=endoscope_markers_path)
+
+    # Render a couple of frames to make sure all changes are visible
+    app.graphicsEngine.render_frame()
+    app.graphicsEngine.render_frame()
+    sleep(0.5)
+    app.graphicsEngine.render_frame()
+    app.graphicsEngine.render_frame()
+    app.graphicsEngine.render_frame()
+    app.graphicsEngine.render_frame()
+    app.graphicsEngine.render_frame()
+    app.graphicsEngine.render_frame()
+
+    # Save file into a temp directory
+    temp_screenshot_file = path.join(tempfile.gettempdir(), 'src-temp-capture-{}.png'.format(np.random.randint(1000)))
+    p3d_filename = Filename(temp_screenshot_file)
+    app.screenshot(temp_screenshot_file, defaultFilename=False)
+
+    # Shutdown the graphics engine
+    app.shutdown()
+    app.destroy()
+
+    # Load the temp file using OpenCV and delete the original file
+    image = cv.imread(temp_screenshot_file)
+    os.remove(temp_screenshot_file)
+    return image
 
 
 def main():
-    app = RenderApp()
-    app.init_scene()
+    # This function is here for testing purposes
+    width, height = 1280, 1024
+    phantom_model_path = '../data/endo_phantom/mesh2_phantom.stl'
+    endoscope_markers_path = '../data/endo_phantom/mesh1_endoscope_markers.stl'
+    T_cam_to_world = np.array([
+        [4.95669255e-01, 8.67596708e-01, -3.98489853e-02, -9.31481022e+01],
+        [-8.68225266e-01, 4.96161724e-01, 2.90365040e-03, -1.21905998e+02],
+        [2.22907387e-02, 3.31586456e-02, 9.99201495e-01, -1.02540324e+02],
+        [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
+    ])
+    R, t = tf.from_transform(T_cam_to_world)
+    cam_matrix = np.array([
+        [321.73571777, 0., 651.79768073],
+        [0., 339.54953003, 514.0065655],
+        [0., 0., 1.]
+    ])
+
+    app = RenderApp(width=width, height=height)
+    app.init_scene(R=R, t=t, cam_matrix=cam_matrix, phantom_model_path=phantom_model_path,
+                   endoscope_markers_path=endoscope_markers_path)
     app.run()
-    # app.graphicsEngine.render_frame()
-    # app.graphicsEngine.render_frame()
-    # file_name = Filename('whatever.png')
-    # app.win.saveScreenshot(file_name)
 
 
 if __name__ == '__main__':
